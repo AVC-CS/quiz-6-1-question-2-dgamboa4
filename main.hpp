a2644994f5f360bcfa0747547a9fbba12b32eb99
dgamboa4 <dgamboa4@students.avc.edu>
2025-04-25 05:46:49 +0000

    save

--- main.hpp
+++ main.hpp
@@ -1,3 +1,34 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ifstream ifs;
+
+    ifs.open("employee.txt");
+    if (!ifs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    int numEmployees;
+    ifs >> numEmployees;
+    cout << "ID\t\tName\t\tDepartment\tSalary" << endl;
+
+    int id;
+    string name, department;
+    double salary, totalSalary = 0;
+
+    while (ifs >> id >> name >> department >> salary) {
+        cout << id << "\t\t" << name << "\t\t" << department << "\t\t" << salary << endl;
+        totalSalary += salary;
+    }
+
+    ifs.close();
+    cout << "Total: " << totalSalary << "Avg: " << totalSalary / numEmployees << endl;
+
+    
+}
